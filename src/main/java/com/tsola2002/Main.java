55de03d40638c9d0ee6fe0f41f2fabc1c6eebe6e
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2023-03-31 13:43:11 +0100

    sorted words in numerical and alphabetical order

--- src/main/java/com/tsola2002/Main.java
+++ src/main/java/com/tsola2002/Main.java
@@ -1,53 +1,22 @@
 package com.tsola2002;
 
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.Arrays;
+import java.util.Comparator;
 
 class Solution {
 
-    public static boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == '(' || c == '{' || c == '[') {
-                stack.push(c);
-            } else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
-                stack.pop();
-            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
-                stack.pop();
-            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
-                stack.pop();
-            } else {
-                return false;
-            }
-        }
-        return stack.isEmpty();
-    }
+
 }
 
 public class Main {
 
     public static void main(String[] args) {
-        // System.out.println("Hello world!");
-//        String s1 = "()";
-//        String s2 = "()[]{}";
-//        String s3 = "(]";
-//        String s4 = "([))";
-//        String s5 = "{[]}";
-//
-//        System.out.println(Solution.isValid(s1)); // true
-//        System.out.println(Solution.isValid(s2)); // true
-//        System.out.println(Solution.isValid(s3)); // false
-//        System.out.println(Solution.isValid(s4)); // false
-//        System.out.println(Solution.isValid(s5)); // true
 
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        if(Solution.isValid(s)){
-            System.out.println("valid");
-        } else {
-            System.out.println("invalid");
-        }
+        String input = "laptop mobile and desktop are not exempt";
+        String[] words = input.split(" ");
+        Arrays.sort(words, Comparator.comparingInt(String::length)
+              .thenComparing(Comparator.naturalOrder()));
+        System.out.println(Arrays.toString(words));
 
     }
 }
